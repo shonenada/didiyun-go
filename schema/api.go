c5cfdae705e478f8d193f46eaddc5bddbbc3f1c1
shonenada <shonenada@gmail.com>
2019-01-23 01:02:36 +0800

    feat: Add Snap schema

--- schema/api.go
+++ schema/api.go
@@ -40,3 +40,5 @@ const ATTACH_EBS_URL = API_BASE + "/storage/ebs/attach"
 const DEATCH_EBS_URL = API_BASE + "/storage/ebs/deatch"
 const CHANGE_NAME_EBS_URL = API_BASE + "/storage/ebs/changeName"
 const CHANGE_SIZE_EBS_URL = API_BASE + "/storage/ebs/changeSize"
+
+const LIST_SNAP_URL = API_BASE + "/storage/snap/list"
