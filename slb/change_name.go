29f2966b8382fcc60d3e90c44562f1571d4d7433
shonenada <shonenada@gmail.com>
2020-12-27 15:33:40 +0800

    feat: Add change slb name

---
+++ slb/change_name.go
@@ -0,0 +1,36 @@
+package slb
+
+import (
+	"encoding/json"
+	"fmt"
+
+	. "github.com/shonenada/didiyun-go/schema"
+)
+
+type ChangeNameRequest struct {
+	Slb []struct {
+		SlbUuid string `json:"slbUuid"`
+		Name    string `json:"name"`
+	} `json:"slb"`
+}
+
+type ChangeNameResponse struct {
+	Errno     int    `json:"errno"`
+	Errmsg    string `json:"errmsg"`
+	RequestId string `json:"requestId"`
+	Data      []Job  `json:"data"`
+}
+
+func (c *Client) ChangeName(request *ChangeNameRequest) (*Job, error) {
+	data, err := json.Marshal(request)
+	if err != nil {
+		fmt.Errorf("Failed to marshal body: %s", err)
+	}
+	body, err := c.HTTPPost(CHANGE_NAME_SLB_URL, data)
+	ret := ChangeNameResponse{}
+	json.Unmarshal(body, &ret)
+	if ret.Errno != 0 {
+		return nil, fmt.Errorf("Failed to request [%s]: %s", ret.RequestId, ret.Errmsg)
+	}
+	return &ret.Data[0], nil
+}
