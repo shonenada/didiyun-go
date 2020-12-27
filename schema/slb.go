82c50d3c38cb52ab2c39fe6d329864a1e8c4ea83
shonenada <shonenada@gmail.com>
2020-12-27 15:33:21 +0800

    refactor: Move common slb struct

--- schema/slb.go
+++ schema/slb.go
@@ -7,3 +7,33 @@ type SlbCondition struct {
 	Dc2Ips   []string `json:"dc2ips,omitempty"`
 	Ips      []string `json:"ips,omitempty"`
 }
+
+type EIP struct {
+	Name           string `json:"name",omitempty`
+	Bandwidth      string `json:"bandwidth"`
+	ChargeWithFlow bool   `json:"chargeWithFlow"`
+}
+
+type Listener struct {
+	Name         string   `json:"listener"`
+	Algorithm    string   `json:"algorithm"`
+	Protocol     string   `json:"protocol"`
+	ListenerPort int      `json:"listenerPort"`
+	BackProtocol string   `json:"backProtocol"`
+	Monitor      Monitor  `json:"monitor"`
+	Members      []Member `json:"members"`
+}
+
+type Monitor struct {
+	Protocol           string `json:"protocol"`
+	Interval           int    `json:"interval"`
+	Timeout            int    `json:"timeout"`
+	HealthyThreshold   int    `json:"healthyThreshold"`
+	UnhealthyThreshold int    `json:"unhealthyThreshold"`
+}
+
+type Member struct {
+	Dc2Uuid string `json:"dc2Uuid"`
+	Weight  int    `json:"weight"`
+	Port    int    `json:"port"`
+}
--- slb/create.go
+++ slb/create.go
@@ -21,36 +21,6 @@ type CreateRequest struct {
 	Listeners    []Listener `json:"listeners"`
 }
 
-type EIP struct {
-	Name           string `json:"name",omitempty`
-	Bandwidth      string `json:"bandwidth"`
-	ChargeWithFlow bool   `json:"chargeWithFlow"`
-}
-
-type Listener struct {
-	Name         string   `json:"listener"`
-	Algorithm    string   `json:"algorithm"`
-	Protocol     string   `json:"protocol"`
-	ListenerPort int      `json:"listenerPort"`
-	BackProtocol string   `json:"backProtocol"`
-	Monitor      Monitor  `json:"monitor"`
-	Members      []Member `json:"members"`
-}
-
-type Monitor struct {
-	Protocol           string `json:"protocol"`
-	Interval           int    `json:"interval"`
-	Timeout            int    `json:"timeout"`
-	HealthyThreshold   int    `json:"healthyThreshold"`
-	UnhealthyThreshold int    `json:"unhealthyThreshold"`
-}
-
-type Member struct {
-	Dc2Uuid string `json:"dc2Uuid"`
-	Weight  int    `json:"weight"`
-	Port    int    `json:"port"`
-}
-
 type CreateResponse struct {
 	Errno     int    `json:"errno"`
 	Errmsg    string `json:"errmsg"`
