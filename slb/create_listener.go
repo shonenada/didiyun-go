1a68385338dd64397ece464ffcf3bd43fffbd1b2
shonenada <shonenada@gmail.com>
2020-12-27 15:33:51 +0800

    feat: add create slb listener

---
+++ slb/create_listener.go
@@ -0,0 +1,27 @@
+package slb
+
+import (
+	"encoding/json"
+	"fmt"
+
+	. "github.com/shonenada/didiyun-go/schema"
+)
+
+type CreateListenerRequest struct {
+	SlbUuid     string     `json:"slbUuid"`
+	SlbListener []Listener `json:"slbListener"`
+}
+
+func (c *Client) CreateListener(request *CreateListenerRequest) (*Job, error) {
+	data, err := json.Marshal(request)
+	if err != nil {
+		fmt.Errorf("Failed to marshal body: %s", err)
+	}
+	body, err := c.HTTPPost(CREATE_LISTENER_SLB_URL, data)
+	ret := ChangeNameResponse{}
+	json.Unmarshal(body, &ret)
+	if ret.Errno != 0 {
+		return nil, fmt.Errorf("Failed to request [%s]: %s", ret.RequestId, ret.Errmsg)
+	}
+	return &ret.Data[0], nil
+}
