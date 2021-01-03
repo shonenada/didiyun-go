a29688f38c8a278ab23cebec678787c625429a73
shonenada <shonenada@gmail.com>
2021-01-03 16:41:55 +0800

    feat: Add list slb example

---
+++ examples/list_slb.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	didiyun "github.com/shonenada/didiyun-go"
+	. "github.com/shonenada/didiyun-go/schema"
+	slb "github.com/shonenada/didiyun-go/slb"
+)
+
+func PrettyPrint(data *[]SlbResponse) {
+	for i, e := range *data {
+		fmt.Printf("[%d] - Uuid: %s\tName: %s\tI$ : %s\tRegion: %s\n", i+1, e.SlbUuid, e.Name, e.Ip, e.Region.Name)
+	}
+}
+
+func main() {
+	accessToken := os.Getenv("DIDIYUN_ACCESS_TOKEN")
+	client := &didiyun.Client{
+		AccessToken: accessToken,
+	}
+	request := slb.ListRequest{
+		RegionId: "gz",
+	}
+
+	if r, e := client.Slb().List(&request); e != nil {
+		fmt.Println(e)
+	} else {
+		PrettyPrint(r)
+	}
+
+}
