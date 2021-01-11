bc617f8b462683e359860aa76d0f064c6c970e2f
shonenada <shonenada@gmail.com>
2021-01-11 13:10:02 +0800

    chore: Add more fields(not list in docs) in EipInfo

--- schema/eip.go
+++ schema/eip.go
@@ -1,12 +1,15 @@
 package schema
 
 type EipInfo struct {
-	Job  Job      `json:"job"`
-	Id   string   `json:"eipId"`
-	Uuid string   `json:"eipUuid"`
-	Ip   string   `json:"ip"`
-	Tags []string `json:"eipTags"`
-	Dc2  struct {
+	Job    Job      `json:"job"`
+	Id     string   `json:"eipId"`
+	Uuid   string   `json:"eipUuid"`
+	Ip     string   `json:"ip"`
+	Tags   []string `json:"eipTags"`
+	State  string   `json:"state"`
+	Status string   `json:"status"`
+	Region Region   `json:"region"`
+	Dc2    struct {
 		Dc2Uuid    string `json:"dc2Uuid"`
 		Name       string `json:name"`
 		Status     string `json:"status"`
